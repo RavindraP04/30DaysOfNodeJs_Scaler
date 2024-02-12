2d78069fa07053789359326209d95c9252950da7
Ravindra Pandey <ravindrapandey99999@gmail.com>
2024-02-12 17:43:44 +0530

    day 12 done

---
+++ day12_rateLimiting/day12_rateLimiting.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 10,
+});
+
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
